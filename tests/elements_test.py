ffa24cc2604f9ce403e5bd09137f92aeac316fca
dd_fomin <dmirtiad.97@gmail.com>
2024-05-24 15:26:47 +0500

    add requirements.txt

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -2,14 +2,15 @@ import time
 
 from pages.elements_page import TextBoxPage
 
+
 class TestElements:
     class TestTextBox:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
-            text_box_page.fill_all_fields()
-            output_name, output_email, output_cur_adr, output_pre_adr = text_box_page.check_filled_form()
-            print(output_name)
-            print(output_email)
-            print(output_cur_adr)
-            print(output_pre_adr)
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            output_name, output_email, output_cur_adr, output_per_adr = text_box_page.check_filled_form()
+            assert full_name == output_name, "the full name does not match"
+            assert email == output_email, "the email does not match"
+            assert current_address == output_cur_adr, "the current address does not match"
+            assert permanent_address == output_per_adr, "the permanent address does not match"
