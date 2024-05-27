0c65b78aad8d8abbf21ce35cecba3e76703961be
dd_fomin <dmirtiad.97@gmail.com>
2024-05-27 17:23:27 +0500

    added test checkboxes

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -1,6 +1,4 @@
-
 class TextBoxPageLocators:
-
     # form field
     FULL_NAME = ("xpath", "//input[@id='userName']")
     EMAIL = ("xpath", "//input[@id='userEmail']")
@@ -14,3 +12,10 @@ class TextBoxPageLocators:
     CREATED_CURRENT_ADDRESS = ("xpath", "//p[@id='currentAddress']")
     CREATED_PERMANENT_ADDRESS = ("xpath", "//p[@id='permanentAddress']")
 
+
+class CheckBoxPageLocators:
+    EXPAND_ALL_BUTTON = ("xpath", "//button[@title='Expand all']")
+    ITEM_LIST = ("xpath", "//span[@class='rct-title']")
+    CHECKED_ITEMS = ("css selector", "svg[class='rct-icon rct-icon-check']")
+    TITLE_ITEM = (".//ancestor::span[@class='rct-text']")
+    OUTPUT_RESULT = ("xpath", "//span[@class='text-success']")
