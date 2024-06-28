8259d099d05e303daf452206003ccd897f0ed325
dd_fomin <dmirtiad.97@gmail.com>
2024-06-28 22:31:16 +0500

    added class:  class TestAlertsPage added test:  test_see_alert  test_alert_appear_5_sec  test_confirm_alert  test_prompt_alert

--- locators/alerts_frame_windows_locators.py
+++ locators/alerts_frame_windows_locators.py
@@ -2,3 +2,11 @@ class BrowserWindowsPageLocators:
     NEW_TAB_BUTTON = ("xpath", "//button[@id='tabButton']")
     TITLE_NEW = ("xpath", "//h1[@id='sampleHeading']")
     NEW_WINDOW_BUTTON = ("xpath", "//button[@id='windowButton']")
+
+class AlertsPageLocators:
+    SEE_ALERT_BUTTON = ("xpath", "//button[@id='alertButton']")
+    APPEAR_ALERT_AFTER_5_SEC_BUTTON = ("xpath", "//button[@id='timerAlertButton']")
+    CONFIRM_BOX_ALERT_BUTTON = ("xpath", "//button[@id='confirmButton']")
+    CONFIRM_RESULT = ("xpath", "//span[@id='confirmResult']")
+    PROMPT_BOX_ALERT_BUTTON = ("xpath", "//button[@id='promtButton']")
+    PROMPT_RESULT = ("xpath", "//span[@id='promptResult']")
--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,4 +1,4 @@
-from pages.alerts_frame_windows_page import BrowserWindowsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
 
 
 class TestAlertsFrameWindows:
@@ -11,3 +11,28 @@ class TestAlertsFrameWindows:
             assert text_result == "This is a sample page", "the new tab has not opened or an incorrect tab has opened"
 
 
+    class TestAlertsPage:
+
+        def test_see_alert(self, driver):
+            alert_page = AlertsPage(driver, "https://demoqa.com/alerts")
+            alert_page.open()
+            alert_text = alert_page.check_see_alert()
+            assert alert_text == "You clicked a button", "Alert did not show up"
+
+        def test_alert_appear_5_sec(self, driver):
+            alert_page = AlertsPage(driver, "https://demoqa.com/alerts")
+            alert_page.open()
+            alert_text = alert_page.check_alert_appear_5_sec()
+            assert alert_text == "This alert appeared after 5 seconds", "Alert did not show up"
+
+        def test_confirm_alert(self, driver):
+            alert_page = AlertsPage(driver, "https://demoqa.com/alerts")
+            alert_page.open()
+            alert_text = alert_page.check_confirm_alert()
+            assert alert_text == "You selected Ok", "Alert did not show up"
+
+        def test_prompt_alert(self, driver):
+            alert_page = AlertsPage(driver, "https://demoqa.com/alerts")
+            alert_page.open()
+            text, alert_text = alert_page.check_prompt_alert()
+            assert text in alert_text, "Alert did not show up"
