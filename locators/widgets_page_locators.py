8388e6b5cb2de0009211d6680f67d886ca06ea67
dd_fomin <dmirtiad.97@gmail.com>
2024-07-02 19:41:30 +0500

    added class:  TestWidgets, TestAccordianPage added tests: - test_accordian

---
+++ locators/widgets_page_locators.py
@@ -0,0 +1,7 @@
+class AccordianPageLocators:
+    SECTION_FIRST = ('xpath', '//div[@id="section1Heading"]')
+    SECTION_CONTENT_FIRST = ('xpath', '//div[@id="section1Content"]//p')
+    SECTION_SECOND = ('xpath', '//div[@id="section2Heading"]')
+    SECTION_CONTENT_SECOND = ('xpath', '//div[@id="section2Content"]//p')
+    SECTION_THIRD = ('xpath', '//div[@id="section3Heading"]')
+    SECTION_CONTENT_THIRD = ('xpath', '//div[@id="section3Content"]//p')
