d482b8ab9e2e8091e72e356b9f06c7c8848fa249
dd_fomin <dmirtiad.97@gmail.com>
2024-07-16 12:12:06 +0500

    created test Tool Tips

--- locators/widgets_page_locators.py
+++ locators/widgets_page_locators.py
@@ -6,12 +6,15 @@ class AccordianPageLocators:
     SECTION_THIRD = ('xpath', '//div[@id="section3Heading"]')
     SECTION_CONTENT_THIRD = ('xpath', '//div[@id="section3Content"]//p')
 
+
 class AutoCompletePageLocators:
     MULTI_INPUT = ('xpath', '//input[@id="autoCompleteMultipleInput"]')
     MULTI_VALUE = ('xpath', '//div[@class="css-1rhbuit-multiValue auto-complete__multi-value"]')
-    MULTI_VALUE_REMOVE = ('xpath', '//div[@class="css-1rhbuit-multiValue auto-complete__multi-value"]//*[local-name() = "path"]')
+    MULTI_VALUE_REMOVE = (
+    'xpath', '//div[@class="css-1rhbuit-multiValue auto-complete__multi-value"]//*[local-name() = "path"]')
     SINGLE_VALUE = ('xpath', '//div[@class="auto-complete__single-value css-1uccc91-singleValue"]')
     SINGLE_INPUT = ('xpath', '//input[@id="autoCompleteSingleInput"]')
+
 
 class DataPickerPageLocators:
     # date
@@ -27,13 +30,16 @@ class DataPickerPageLocators:
     DATE_AND_TIME_MONTH_LIST = ('xpath', '//div[contains(@class, "react-datepicker__month-option")]')
     DATE_AND_TIME_YEAR_LIST = ('xpath', '//div[contains(@class, "react-datepicker__year-option")]')
 
+
 class SliderPageLocators:
     INPUT_SLIDER = ('xpath', '//input[@class="range-slider range-slider--primary"]')
     SLIDER_VALUE = ('xpath', '//input[@id="sliderValue"]')
 
+
 class ProgressBarPageLocators:
     PROGRESS_BAR_BUTTON = ('xpath', '//button[@id="startStopButton"]')
     PROGRESS_BAR_VALUE = ('xpath', '//div[@class="progress-bar bg-info"]')
+
 
 class TabsPageLocators:
     TABS_WHAT = ('xpath', '//a[@id="demo-tab-what"]')
@@ -44,3 +50,19 @@ class TabsPageLocators:
     TABS_USE_CONTENT = ('xpath', '//div[@id="demo-tabpane-use"]')
     TABS_MORE = ('xpath', '//a[@id="demo-tab-more"]')
     TABS_MORE_CONTENT = ('xpath', '//div[@id="demo-tabpane-more"]')
+
+
+class ToolTipsPageLocators:
+    BUTTON = ('xpath', '//button[@id="toolTipButton"]')
+    TOOL_TIP_BUTTON = ('xpath', '//button[@aria-describedby="buttonToolTip"]')
+
+    FIELD = ('xpath', '//input[@id="toolTipTextField"]')
+    TOOL_TIP_FIELD = ('xpath', '//input[@aria-describedby="textFieldToolTip"]')
+
+    CONTRARY_LINK = ('xpath', '//a[text()="Contrary"]')
+    TOOL_TIP_CONTRARY= ('xpath', '//a[@aria-describedby="contraryTexToolTip"]')
+
+    SECTION_LINK = ('xpath', '//a[text()="1.10.32"]')
+    TOOL_TIP_SECTION = ('xpath', '//a[@aria-describedby="sectionToolTip"]')
+
+    TOOL_TIPS_INNERS = ('xpath', '//div[@class="tooltip-inner"]')
