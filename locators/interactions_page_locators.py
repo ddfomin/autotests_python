56ce6495d1f9720b3e2ef44b49b44e44897d1b38
dd_fomin <dmirtiad.97@gmail.com>
2024-08-20 14:03:26 +0500

    created class Droppable created tests for Droppable page

--- locators/interactions_page_locators.py
+++ locators/interactions_page_locators.py
@@ -13,8 +13,36 @@ class SelectablePageLocators:
     GRID_ITEM = ('xpath', '//li[@class="list-group-item list-group-item-action"]')
     GRID_ITEM_ACTIVE = ('xpath', '//li[@class="list-group-item active list-group-item-action"]')
 
+
 class ResizablePageLocators:
     RESIZABLE_BOX_HANDLE = ('xpath', '(//span[@class="react-resizable-handle react-resizable-handle-se"])[1]')
     RESIZABLE_BOX = ('xpath', '//div[@id="resizableBoxWithRestriction"]')
     RESIZABLE_HANDLE = ('xpath', '(//span[@class="react-resizable-handle react-resizable-handle-se"])[2]')
     RESIZABLE = ('xpath', '//div[@id="resizable"]')
+
+
+class DroppablePageLocators:
+    # Simple
+    SIMPLE_TAB = ('xpath', '//a[@id="droppableExample-tab-simple"]')
+    DRAG_ME_SIMPLE = ('xpath', '//div[@id="draggable"]')
+    DROP_HERE_SIMPLE = ('xpath', '//div[@id="simpleDropContainer"]/div[@id="droppable"]')
+
+    # Accept
+    ACCEPT_TAB = ('xpath', '//a[@id="droppableExample-tab-accept"]')
+    ACCEPTABLE = ('xpath', '//div[@id="acceptable"]')
+    NOT_ACCEPTABLE = ('xpath', '//div[@id="notAcceptable"]')
+    DROP_HERE_ACCEPT = ('xpath', '//div[@id="acceptDropContainer"]/div[@id="droppable"]')
+
+    # Prevent Propogation
+    PREVENT_TAB = ('xpath', '//a[@id="droppableExample-tab-preventPropogation"]')
+    NOT_GREEDY_DROP_BOX_TEXT = ('xpath', '//div[@id="notGreedyDropBox"]/p')
+    NOT_GREEDY_DROP_INNER_BOX = ('xpath', '//div[@id="notGreedyInnerDropBox"]')
+    GREEDY_DROP_BOX_TEXT = ('xpath', '//div[@id="greedyDropBox"]/p')
+    GREEDY_INNER_BOX = ('xpath', '//div[@id="greedyDropBoxInner"]')
+    DRAG_ME_PREVENT = ('xpath', '//div[@id="dragBox"]')
+
+    # Revert Draggable
+    REVENT_TAB = ('xpath', '//a[@id="droppableExample-tab-revertable"]')
+    WILL_REVERT = ('xpath', '//div[@id="revertable"]')
+    NOT_REVERT = ('xpath', '//div[@id="notRevertable"]')
+    DROP_HERE_REVERT = ('xpath', '//div[@id="revertableDropContainer"]/div[@id="droppable"]')
--- pages/interactions_page.py
+++ pages/interactions_page.py
@@ -1,7 +1,8 @@
 import random
 import time
 
-from locators.interactions_page_locators import SortablePageLocators, SelectablePageLocators, ResizablePageLocators
+from locators.interactions_page_locators import SortablePageLocators, SelectablePageLocators, ResizablePageLocators, \
+    DroppablePageLocators
 from pages.base_page import BasePage
 
 
@@ -81,3 +82,53 @@ class ResizablePage(BasePage):
                                             random.randint(-25, -2), random.randint(-25, -2))
         min_size = self.get_px_from_width_height(self.get_max_min_size(self.locators.RESIZABLE))
         return max_size, min_size
+
+
+class DroppablePage(BasePage):
+    locators = DroppablePageLocators()
+
+    def drop_simple(self):
+        self.element_is_visible(self.locators.SIMPLE_TAB).click()
+        drag_div = self.element_is_visible(self.locators.DRAG_ME_SIMPLE)
+        drop_div = self.element_is_visible(self.locators.DROP_HERE_SIMPLE)
+        self.action_drag_and_drop_by_element(drag_div, drop_div)
+        return drop_div.text
+
+    def drop_accept(self):
+        self.element_is_visible(self.locators.ACCEPT_TAB).click()
+        acceptable_div = self.element_is_visible(self.locators.ACCEPTABLE)
+        not_acceptable_div = self.element_is_visible(self.locators.NOT_ACCEPTABLE)
+        drop_div = self.element_is_visible(self.locators.DROP_HERE_ACCEPT)
+        self.action_drag_and_drop_by_element(not_acceptable_div, drop_div)
+        drop_div_text_not_acceptable = drop_div.text
+        self.action_drag_and_drop_by_element(acceptable_div, drop_div)
+        drop_div_text_acceptable = drop_div.text
+        return drop_div_text_not_acceptable, drop_div_text_acceptable
+
+    def drop_prevent_propogation(self):
+        self.element_is_visible(self.locators.PREVENT_TAB).click()
+        drag_div = self.element_is_visible(self.locators.DRAG_ME_PREVENT)
+        not_greedy_inner_box = self.element_is_visible(self.locators.NOT_GREEDY_DROP_INNER_BOX)
+        greedy_inner_box = self.element_is_visible(self.locators.GREEDY_INNER_BOX)
+        self.action_drag_and_drop_by_element(drag_div, not_greedy_inner_box)
+        text_not_greedy_box = self.element_is_visible(self.locators.NOT_GREEDY_DROP_BOX_TEXT).text
+        text_not_greedy_inner_box = not_greedy_inner_box.text
+        self.action_drag_and_drop_by_element(drag_div, greedy_inner_box)
+        text_greedy_box = self.element_is_visible(self.locators.GREEDY_DROP_BOX_TEXT).text
+        text_greedy_inner_box = greedy_inner_box.text
+        return text_not_greedy_box, text_not_greedy_inner_box, text_greedy_box, text_greedy_inner_box
+
+    def drop_revert_draggable(self, type_drag):
+        drag = {
+            'will':
+                {'revert': self.locators.WILL_REVERT},
+            'not_will': {'revert': self.locators.NOT_REVERT},
+        }
+        self.element_is_visible(self.locators.REVENT_TAB).click()
+        revert = self.element_is_visible(drag[type_drag]['revert'])
+        drop_div = self.element_is_visible(self.locators.DROP_HERE_REVERT)
+        self.action_drag_and_drop_by_element(revert, drop_div)
+        position_after_move = revert.get_attribute('style')
+        time.sleep(2)
+        position_after_revent = revert.get_attribute('style')
+        return position_after_move, position_after_revent
